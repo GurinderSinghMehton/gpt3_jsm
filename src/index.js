13ff73145cee406c4a94ebd3ae0423babdd34b5e
Gurinder Singh <156795784+GurinderSinghMehton@users.noreply.github.com>
2024-03-25 22:25:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import './index.css';
+const root = createRoot(document.getElementById('root'))
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
